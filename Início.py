4c703508fb0df2985f104ff427fe08184137dd32
MathewDiniz <matheusdinizz_@hotmail.com>
2024-01-29 22:01:23 -0300

    Change back again

--- Início.py
+++ Início.py
@@ -30,8 +30,8 @@ if "df_data" not in st.session_state:
     ipea = GetIPEAData(database_path="source\db_main.csv")
     st.session_state.df_data = ipea.db_main
 
-st.session_state.db_war_contry = pd.read_csv('war_data\countries-in-conflict-data-all.csv', sep=",")
-st.session_state.db_war_type = pd.read_csv('war_data\countries-in-conflict-data-by-type.csv', sep=",")
+st.session_state.db_war_contry = pd.read_csv('countries-in-conflict-data-all.csv', sep=",")
+st.session_state.db_war_type = pd.read_csv('countries-in-conflict-data-by-type.csv', sep=",")
 
 
 # Análisar em que página fica
